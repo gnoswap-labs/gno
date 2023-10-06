8aa702c29fc1521463ab8dadfc35dc7330ca56d7
n3wbie <r3v4@onbloc.xyz>
2023-10-06 14:27:58 +0900

    fix: skip 1 block == skip 5 seconds

--- gnovm/tests/imports.go
+++ gnovm/tests/imports.go
@@ -731,7 +731,7 @@ func testPackageInjector(store gno.Store, pn *gno.PackageNode) {
 
 				ctx := m.Context.(stdlibs.ExecContext)
 				ctx.Height += count
-				ctx.Timestamp += (count * 2) // skip 1 block > skip 2 seconds
+				ctx.Timestamp += (count * 5) // skip 1 block > skip 5 seconds
 				m.Context = ctx
 			},
 		)
